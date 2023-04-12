cb0163a7ff117afc4be302068584954fff8984a2
nocturnal_hum <thanin.chan@gmail.com>
2023-04-12 03:28:36 -0400

    Removed relative class from Hourly component

--- components/ForecastWeather.jsx
+++ components/ForecastWeather.jsx
@@ -3,7 +3,7 @@ import HumidityPressure from './HumidityPressure';
 
 export default function ForecastWeather({ meteoData }) {
   return (
-    <div className='relative h-[400px] max-w-3xl rounded-2xl border-[2px] border-t-[1px] border-b-gray-600/80 border-l-gray-700/50 border-r-gray-400/80 border-t-gray-100/50'>
+    <div className='relative h-[800px] max-w-3xl rounded-2xl border-[2px] border-t-[1px] border-b-gray-600/80 border-l-gray-700/50 border-r-gray-400/80 border-t-gray-100/50'>
       <div className='h-full w-full p-2'>
         <HumidityPressure
           humidity={1}
