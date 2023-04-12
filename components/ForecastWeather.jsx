49623c8e3406608d92359c3239beddd3a4237bb7
nocturnal_hum <thanin.chan@gmail.com>
2023-04-12 03:02:03 -0400

    Adjust Forecast Glass Height to debug scroll issue

--- components/ForecastWeather.jsx
+++ components/ForecastWeather.jsx
@@ -3,7 +3,7 @@ import HumidityPressure from './HumidityPressure';
 
 export default function ForecastWeather({ meteoData }) {
   return (
-    <div className='relative w-full max-w-3xl rounded-2xl border-[2px] border-b-[1px] border-b-gray-500   border-l-gray-700/50 border-r-gray-400/80 border-t-gray-500/50'>
+    <div className='relative h-[800px] max-w-3xl rounded-2xl border-[2px] border-t-[1px] border-b-gray-600/80   border-l-gray-700/50 border-r-gray-400/80 border-t-gray-100/50'>
       <div className='h-full w-full p-2'>
         <HumidityPressure
           humidity={1}
