d461d68923dbcd3ad7900e2e86224e2063a8e98f
nocturnal_hum <thanin.chan@gmail.com>
2023-04-17 21:51:37 -0400

    Added night background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,7 @@ module.exports = {
         pacific:
           "url('https://images.unsplash.com/photo-1511884642898-4c92249e20b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80')",
         snow: "url('/backgrounds/snow.jpg')",
+        night: "url('/backgrounds/night.jpg')",
       },
       animation: {
         'spin-slow': 'spin 2.5s ease-out infinite',
